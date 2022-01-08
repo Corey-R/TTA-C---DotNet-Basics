411277f8f0fdb00accb38a250ff421380ee556d2
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-08 14:18:11 -0600

    Finished Video Tutorial on Variables and Data Types and Created a Daily Report Program

---
+++ Basic_C#_Programs/Assignments/DailyReport/DailyReport/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // daily report program
+            // -------------------------
+
+            Console.WriteLine("The Tech Academy. \nStudent Daily Report\n");
+
+            // the "\n" are used to start the next items on a new line...
+
+            Console.WriteLine("\n1. What is your name?");
+            string yourName = Console.ReadLine(); // storing string value of user input here
+
+            Console.WriteLine("\n2. What course are you on?");
+            // the students' answer will be stored in this string variable
+            string courseName = Console.ReadLine();
+
+            Console.WriteLine("\n3. What Page Number?");
+            // Though asking for a number, Console.ReadLine() only accepts...
+            // string values and since no math is needed it will remain a string
+            string pageNumber = Console.ReadLine();
+
+            Console.WriteLine("\n4. Do you need help with anything? Please answer \"True\" or \"False\".");
+            string needHelp = Console.ReadLine();
+            // NOTE: to convert to boolean if needed...
+            // bool helpAnswer = Convert.ToString(needHelp);
+
+            Console.WriteLine("\n5. Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveXp = Console.ReadLine();
+
+            Console.WriteLine("\n6. Is there any other feedback you'd like to provide? Please be specific.");
+            string anyFeedback = Console.ReadLine();
+
+            Console.WriteLine("\n7. How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+
+            Console.WriteLine("\n\nThank you for your answers. An instructor will respond to this shortly. \nHave a great day!");
+            Console.Read();
+            
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/Video_Tutorial/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            // This is a simple program 
+
+            //Console.WriteLine("what is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("your name is: " + yourName);
+            //Console.Read();
+
+            // This program uses variables, casting, and basic math logic
+
+            //Console.WriteLine("What is your favorite number?");
+            // NOTE: Console.ReadLine() only takes string values 
+            // Therefore, casting (or converting data types) is used
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.Read();
+
+            // Various Data Types 
+            bool isStudying = false; // Boolean data type
+            byte hoursWorked = 42; // Byte data type represents int from 0 to 255
+            sbyte currentTemperature = -23; // sbyte (or signed byte) data type
+            char questionMark = '\u2103'; // Char data type (a single unicode character)
+
+            decimal moneyInBank = 100.5m; // Decimal data type requires "m" suffix 
+
+            double heightInCentimeters = 211.282803584; // Double data type
+
+            float secondsLeft = 2.62f; // Float data type requires "f" suffix or error
+
+            short temperatureOnMars = -341; // Short Data Type (-32,000 to 32,000)
+
+            string myName = "jesse"; // String data type
+
+            // CASTING EXAMPLES 
+
+            // Convert to string
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString(); // Syntax used to convert to string
+
+            // Convert to boolean
+            bool isRaining = true;
+            // Another method of converting to string 
+            string rainingStatus = Convert.ToString(isRaining);
+
+
+
+            Console.WriteLine(rainingStatus);
+            Console.Read();
+        }
+    }
+}
