c170bf4acd66b006af63aabb8076c0dccf4afb05
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-09 13:27:03 -0600

    Completed Video Tutorial explaining Boolean Operators and created a functioning Insurance Approval project for submission review.

---
+++ Basic_C#_Programs/Assignments/InsuranceApproval/InsuranceApproval/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // INSURANCE APPROVAL PROJECT
+            //----------------------------------
+
+            Console.WriteLine("Please answer these following questions: \n");
+
+            // Application Questions
+            //-----------------------------
+            // Using Console to gather user's raw data for later 
+
+            Console.WriteLine("\nHow old are you? ");
+            // storing user's age inside variable "Age"
+            string Age = Console.ReadLine();
+
+            Console.WriteLine("\ntrue or false: Ever had a DUI?");
+            string DUI = Console.ReadLine();
+
+            Console.WriteLine("\nHow many speeding tickets do you have? Please enter the number.");
+            string speedTicket = Console.ReadLine();
+
+            // QUALIFICATION SECTION
+            // Using Boolean Logic to determine if the applicant
+            // is qualfied. 
+
+            // Applicant must be over 15
+            // convert string Age to an int data type
+            int applicantAge = Convert.ToInt32(Age);
+            // check if user'a age is over 15
+            bool over15 = applicantAge > 15;
+
+            // No DUIs 
+            // convert string DUI to boolean
+            bool chkDUIs = Convert.ToBoolean(DUI);
+            // If the user does NOT have any DUIs (or false)
+            // set noDUIs value to "true"
+            bool noDUIs = chkDUIs != true;
+
+            // No more than 3 speeding tickets
+            // convert string speedTicket to int
+            int spdTick = Convert.ToInt32(speedTicket);
+            // set bool to true if user has less than or equal 
+            // to 3 speeding tickets
+            bool over3Ticks = spdTick <= 3;
+
+            // PUT IT ALL TOGETHER
+            // create a Qualified boolean variable using
+            // AND && Operator 
+
+            bool qualified = over15 && noDUIs && over3Ticks;
+
+            // Print results in the console 
+            Console.WriteLine("\n\nQualified? \n" + qualified);
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/Video_Tutorial/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND BOOLEAN LOGIC && (AND) OPERATOR
+            //-------------------------------------
+
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            // OR BOOLEAN LOGIC || (OR) OPERATOR
+            //-------------------------------------
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            // EQUALS BOOLEAN LOGIC == (EQUIVALENT) OPERATOR
+            //-------------------------------------------------
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            // NOT EQUALS BOOL LOGIC != (NOT EQUALS) OPERATOR
+            //----------------------------------------------------
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            // XOR OPERATOR ^ 
+            //----------------------------
+            // NOTE: Only evaluates to true if one is true
+            // BUT not BOTH
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
