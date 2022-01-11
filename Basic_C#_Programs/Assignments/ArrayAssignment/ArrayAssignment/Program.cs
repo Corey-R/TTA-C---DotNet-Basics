12005097fe896fa536401d803c8202e9615f5e61
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-11 13:55:44 -0600

    Array Video Tutorial and Submission Assignment

---
+++ Basic_C#_Programs/Assignments/ArrayAssignment/ArrayAssignment/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        // ARRAYS SUBMISSION ASSIGNMENT
+        //==============================================
+
+        // print welcome message to the console...
+        Console.WriteLine("Hi there, why don't we play a guessing game. \nHere, in this game you will be asked a series of questions and depending on which number you provide will \ndetermine your outcome. Good Luck!");
+
+        // String Array
+        //------------------
+        // create the array of strings...
+        string[] fruitArray = { "Apples", "Bananas", "Cherries", "Grapes", "Mangos", "Peaches", "Pears", "Strawberries", "Tomatoes", "Watermelons" };
+        // creating a boolean variable for later use
+        bool go = true;
+
+        // print user instructions to console...
+        Console.WriteLine("\n\nPlease enter a number 0 to 9.");               
+
+        // while true, store user input into int variable and run branching statement...
+        while (go)
+        {
+            int fruitNum = Convert.ToInt32(Console.ReadLine());
+            // if the user met conditions, print index of array content to the console...
+            if (fruitNum >= 0 && fruitNum <= 9)
+            {
+                Console.WriteLine("The fruit you selected was: \"" + fruitArray[fruitNum] + "\"");
+                // this stops the loop
+                go = false;
+            }
+            // otherwise, print an error message allowing the user another chance to make corrections..
+            else
+            {
+                Console.WriteLine("Please enter a single digit number!");               
+            }
+        }
+        
+        // NOTE: Very similar code below so refer to above comments as reference... 
+
+        // Integer Array
+        //--------------------
+
+        int[] intArray = { 2300, 1, 900, 10000, 30050090, 20, 5 };
+        Console.WriteLine("\n\nNow, pick a number 0 to 6: ");
+        bool start = true;
+
+        while (start)
+        {
+            int guessedNum = Convert.ToInt32(Console.ReadLine());
+            if (guessedNum >= 0 && guessedNum <= 6)
+            {
+                Console.WriteLine("Nice! Looks like you'll make " + intArray[guessedNum] + " dollars in the next month! \nYou know, sharing is caring...");
+                start = false;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a single digit number 0 to 6!");
+            }
+        }
+
+        // String List
+        //-----------------
+
+        // this crates a list and adds values to the list
+        List<string> stringList = new List<string>();
+        stringList.Add("Welcome back!");
+        stringList.Add("Good morning!");
+        stringList.Add("That's my purse...I don't know you!");
+        stringList.Add("Better watch your back!");
+        stringList.Add("Winner! You won this message. Enjoy!");
+
+        // print instructions
+        Console.WriteLine("\n\nFinally, enter a number 0 to 4: ");
+        bool set = true;
+
+        // while true, perform the following..
+        while (set)
+        {
+            int randomNum = Convert.ToInt32(Console.ReadLine());
+            if (randomNum >= 0 && randomNum <= 4)
+            {
+                Console.WriteLine(stringList[randomNum]);
+                set = false;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a single digit number 0 to 4!");
+            }
+        }
+
+        // print thank you for playing message
+        Console.WriteLine("\n\nThanks for playing and being a great sport! See you next time!");
+
+        // keeps the console open until user closes...
+        Console.Read();
+    }
+}
+
---
+++ Basic_C#_Programs/Video_Tutorial/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        // ARRAYS AND LISTS
+        //=========================
+
+        // Use Square Brackets [] when referring to arrays 
+        // dataType[] variableName = new dataType[]; is required when using arrays
+
+        // You have to define the size of the array before instantiating it
+
+        // Below is the syntax stating 
+        // integer array called "numArray" equals a new integer array that has
+        // 5 numbers in it.
+        int[] numArray = new int[5];
+
+        // Now, you must individually give the numeric value in each index of the
+        // array.
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 200;
+        numArray[4] = 5000;
+
+        // THERE'S A BETTER WAY....
+
+        // Instead of placing the size amount, add curly braces and enter your 
+        // values when defining the array.
+        int[] numArray1 = new int[]
+        {
+            5, 2, 10, 200, 5000, 600, 2300
+        };
+
+        // THERE'S AN EVEN FASTER WAY
+        //---------------------------------------
+
+        int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300};
+
+        // Change an array item by specifying the index
+        // NOTE: You are limited to the size of the array once it has been 
+        // defined with its values.
+        numArray2[5] = 650;
+
+
+        // LISTS 
+        //=====================
+
+        // List<dataType> variableName = new List<dataType>(); is the syntax
+        // NOTE: Size amount does not have to be specified when defining the list.
+        List<int> intList = new List<int>();
+
+        // .Add() adds values to the list
+        intList.Add(4);
+        intList.Add(10);
+
+        // .Remove() removes a value from the list
+        intList.Remove(10);
+
+        // String list example
+        List<string> stringList = new List<string>();
+        stringList.Add("Hello");
+        stringList.Add("Jesse");
+        stringList.Remove("Jesse");
+
+        // Print to console to check
+        Console.WriteLine(stringList[0]);
+        Console.Read();
+
+    }
+}
+
