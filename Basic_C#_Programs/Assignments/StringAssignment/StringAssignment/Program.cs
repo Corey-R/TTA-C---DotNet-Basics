b290bb75f1d42fc5417d76da65f8db20e5b0fb46
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-10 22:33:17 -0600

    String Function video tutorial and String Assignment completed.

---
+++ Basic_C#_Programs/Assignments/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // STRING ASSIGNMENT
+            //========================
+
+            // String Concatenation 
+            //--------------------------
+
+            // Concatenate 3 strings
+            // NOTE: Space added at the end of fName and mName to show
+            // "Corey Antonio Reid" instead of "CoreyAntonioReid"
+            string fName = "Corey ";
+            string mName = "Antonio ";
+            string lName = "Reid";
+
+            string fullName = fName + mName + lName;
+
+            // Printing full name to console in UPPERCASE
+            Console.WriteLine("My full name is: " + fullName.ToUpper());
+
+            // StringBuilding A Paragraph
+            //----------------------------------
+
+            // Use StringBuilder to create a paragraph one sentence at a time.
+            StringBuilder paragraph = new StringBuilder();
+
+            // Use .Append() to add to the StringBuilder variable "paragraph"
+            paragraph.Append("\n\n\tThis paragraph is being created using StringBuilder. ");
+            paragraph.Append("StringBuilder is a function that allows us to add \ninformation to the same strin variable. ");
+            paragraph.Append("Since strings are immutable, it cannot be changed by simply setting \nanother value to it. ");
+            paragraph.Append("It's for this reason, why it's recommended to use the StringBuilder function.");
+
+            Console.WriteLine(paragraph);
+            Console.Read();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/Video_Tutorial/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jesse";
+            string quote = "The man said, \"Hello\", Jesse. \nHello on a new line. \n\tHello on a tab.";
+            
+            // Use the @ sign to indicate everything within the quotations are 
+            // part of the string (which means no escape symbols, or backslashes,
+            // are needed.
+            string filename = @"C:\Users\Jesse";
+
+            // STRING FUNCTIONS 
+            //=======================
+
+            // Does the value stored in "string name" contain the letter s?
+            bool trueOrFalse = name.Contains("s");
+
+            // TrueOrFalse = does name end with "s"
+            trueOrFalse = name.EndsWith("s");
+
+            // name.length = Number of characters in string value "Jesse"
+            int length = name.Length;            
+
+            // name.ToUpper() = Places the string value in ALL CAPS ("JESSE")
+            // name.ToLower() = Places the string value in all lowercase ("jesse")
+            name = name.ToLower();
+
+            // StringBuilder allows devs to add to a string value without changing
+            // its contents (SideNote: Strings are immutable 
+
+            StringBuilder sb = new StringBuilder();
+
+            // Append() allows you to add string value to stored variable
+            sb.Append("My name is Jesse.");
+
+            Console.WriteLine(sb);
+            Console.Read();
+        }
+    }
+}
