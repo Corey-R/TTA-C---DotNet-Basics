eb162754ad6ec06a41b39324d12d354f2322c068
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-12 20:57:23 -0600

    Exception Handling video tutorial and assignment completed

---
+++ Basic_C#_Programs/Assignments/StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // STRINGS AND INTEGERS SUBMISSION ASSIGNMENT
+            //==================================================================
+
+            // creating a list of integers
+            List<int> dividends = new List<int>() { 19, 33, 47, 1 };
+
+            // prompt user to enter a number and store their input
+            Console.WriteLine("Please enter a number.");
+                        
+
+            // adding a try/catch block to catch exceptions
+            try
+            {
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nYour results: ");
+                // create for loop that iterates the number list and divides each by the user's number
+                foreach (int dividend in dividends)
+                {
+                    int quotient = dividend / divisor;
+                    Console.WriteLine("\n" + dividend + " / " + divisor + " = " + quotient);
+                }
+            }
+            catch (DivideByZeroException ex) // catches when user enters 0
+            {
+                // prints custom error message to console
+                Console.WriteLine("\nThis calculation cannot be performed. Do not enter '0'.");
+            }
+            catch (FormatException ex) // catches when user enters letters instead of numbers
+            {
+                Console.WriteLine("\nYou must enter a valid number. No letters (e.g. 8, 17, 5, etc.)");
+            }
+            catch (Exception ex) // catches any general exceptions
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // performed outside the try/catch block (will display regardless of the outcome)
+            finally
+            {
+                // print to console that try/catch block has ended and program will continue
+                Console.WriteLine("\n\nTry/catch block has ended.. \nContinuing Program...");
+                Console.Read(); // keeps console open 
+            }           
+        }
+    }
+}
---
+++ Basic_C#_Programs/Video_Tutorial/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // try this code
+        try
+        {
+            Console.WriteLine("Pick a number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + "divided by " + numberTwo + "= " + numberThree);           
+        }
+        // catch any format errors (data type ex is for Format Exception)
+        catch (FormatException ex)
+        {
+            // print exception message to console if thrown
+            Console.WriteLine("Please enter a whole number.");
+            return; // returns user to another page (exits the console)
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        // creates a general exception that catches all exceptions thrown
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        // finally placed at end of the try catch block
+        // NOTE: if the try block runs perfectly with no exceptions, finally will still run.
+        // NOTE: if there was an exception thrown, the finally block will still run
+        finally
+        {
+            Console.Read();
+        }
+        
+    }
+}
+
