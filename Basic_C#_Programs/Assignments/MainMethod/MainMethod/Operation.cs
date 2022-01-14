7febbb59da97747c25bf13511d485301b63a064d
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-13 18:25:54 -0600

    Added methods, out variables, and parameters to TwentyOne via video tutorial. Completed MainMethod assignment for review.

---
+++ Basic_C#_Programs/Assignments/MainMethod/MainMethod/Operation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Operation
+    {
+        // creating method that passes in an integer
+        public int Math(int number)
+        {
+            // storing results in int variable "added"
+            int added = number + 30;
+            return added; // returning results to whoever called it
+        }
+        // creating method with same name but different parameter 
+        public int Math(decimal number)
+        {
+            int multiplied = Convert.ToInt32(number) * 10;
+            return multiplied;
+        }
+        // same name method, different parameter
+        public int Math(string number)
+        {
+            int subtracted = Convert.ToInt32(number) - 100;
+            return subtracted;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Assignments/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating the class "Operation" as a new object
+            Operation operate = new Operation();
+            // Calling the 3 methods and passing their respected arguments
+            int results = operate.Math(5);
+            int resultsTwo = operate.Math(25.00m);
+            int resultsThree = operate.Math("300");
+            // printing results to the console
+            Console.WriteLine(results + "\n" + resultsTwo + "\n" + resultsThree);
+            Console.Read();
+        }
+    }
+}
