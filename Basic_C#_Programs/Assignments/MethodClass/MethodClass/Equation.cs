9494409a29a8ee43ade5ebaad058d5fa4e908076
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-13 20:45:33 -0600

    Completed MethodClass assignment

---
+++ Basic_C#_Programs/Assignments/MethodClass/MethodClass/Equation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class Equation
+    {
+        // creating a void method that takes two integer parameters
+        public void Multiply(int number, out int display)
+        {            
+            // performing math operation on first integer
+            int x = number * 200;
+            // passing a value to second integer
+            // NOTE: "out int variableName" must be present in both defining and calling the method
+            display = 17;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Assignments/MethodClass/MethodClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating Equation class
+            Equation equate = new Equation();
+            // calling Multiply method passing in respected arguments
+            equate.Multiply(25, out int display);
+
+            // calling Multiply method with named variables of their respected arguments
+            // NOTE: uncomment (ctrl + k + u) the below code 
+            // equate.Multiply(number: 25, display: out int display);
+
+            // prints out the second integer as requested 
+            Console.WriteLine(display);
+            Console.Read();
+        }
+    }
+}
