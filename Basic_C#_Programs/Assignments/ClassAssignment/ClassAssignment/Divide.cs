b2c4c38441d5a17d2d2a674e7dc1f0a32f84d953
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-14 18:31:08 -0600

    Completed Class submission assignment

---
+++ Basic_C#_Programs/Assignments/ClassAssignment/ClassAssignment/Divide.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    // made Divide class public and static
+    // public allows access to other programs to use this class
+    // static allows other programs to use this class without instantiating an object first
+    public static class Divide
+        // NOTE: if class is static, remember to make all class members and methods static as well
+    {
+        // creating a static method dividing the data by 2
+        public static void Quotient(int userNumber, out int number)
+        {            
+            number = userNumber / 2;
+        }
+
+        // creating a static method with output parameters
+        public static void Example(out string thanks, string name)
+        {
+            thanks = "\n\nThank you, " + name + " for your participation!";            
+        }
+
+        // creating an overloading static method 
+        // the difference is the number of parameters it requires
+        public static void Example(out string welcome)
+        {
+            welcome = "Hello, this is a division program.";
+        }
+    }
+}
---
+++ Basic_C#_Programs/Assignments/ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // NOTE: for static classes, you don't have to instantiate it first before using it.
+            // This method was called to pass in a welcome message
+            Divide.Example(out string welcome);
+            // prints welcome message to console
+            Console.WriteLine(welcome);
+            // Asks for user's name for later use
+            Console.WriteLine("What's your name: ");
+            string name = Console.ReadLine(); // stores user's name here
+            // Asks user to enter a number 
+            Console.WriteLine("\nPlease enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine()); // stores user's number here
+
+            // calls Quotient method to perform division operation on user's number and..
+            // passes the results to the output variable "number"
+            Divide.Quotient(userNumber, out int number);
+            // an overload method that uses two parameters, an output variable and an input variable
+            Divide.Example(out string thanks, name);
+            
+            // prints the user's results to the console using the output variable from Quotient method
+            Console.WriteLine("\nYour results: \n" + userNumber + " divided by 2 = " + number);
+            // prints a thank you message to console using output variable from overloaded method
+            Console.WriteLine(thanks);
+            Console.Read();
+        }
+    }
+}
