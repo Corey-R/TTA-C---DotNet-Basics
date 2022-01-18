245e4ac423158c24daaff5fc5cf01282001b0f9a
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2022-01-18 17:51:31 -0600

    Struct example.

---
+++ Basic_C#_Programs/Assignments/Struct/Struct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            Number number2 = number;
+            number.Amount = 175m;
+            number2.Amount = 200m;
+
+            Console.WriteLine("Number 1 = {0} and Number 2 = {1}. \nThese have their own values because \"Number\" is a struct.", number.Amount, number2.Amount);
+            Console.Read();
+        }
+    }
+}
--- Basic_C#_Programs/Video_Tutorial/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/Video_Tutorial/TwentyOne/TwentyOne/Program.cs
@@ -8,6 +8,8 @@ namespace TwentyOne
 {
     class Program
     {        
+        
+            
         // methods include:
         // access modifier (public), static if there is no instance of the class (static), 
         // return type (void = return nothing), name of method (Main()), and parameters if needed
@@ -17,17 +19,20 @@ namespace TwentyOne
 
         static void Main(string[] args)
         {
-
+            Card card1 = new Card();
+            Card card2 = card1;
+            card1.Face = Face.King;
+            card2.Face = Face.Queen;
             // instantiate a Deck of cards
             // Data Type "Deck" named Deck equals a new Deck() object
-            Deck deck = new Deck();
-            deck.Shuffle(3);
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
             Console.Read();
         }            
        
